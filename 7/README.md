333ee7131d4bf5bed1117e7b8d8e1e937df831cf
Журавский Матвей Антонович <77049921+textil24@users.noreply.github.com>
2021-12-20 13:42:03 +0300

    Add files via upload

---
+++ 7/README.md
@@ -0,0 +1,1 @@
+<h1><a href="https://newsarcasmprovider.github.io/exercise_07.12/">GitHub Pages</a></h1>
---
+++ 7/precache-manifest.ccf6077e3131eac1a622386ebb3dad50.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c755287399e90f67fd12f1c5ab643ebd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c5c4d39fcb6f4a984ba",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "8ae49aac9dab507a9ba8",
+    "url": "/static/js/2.18b2a2a8.chunk.js"
+  },
+  {
+    "revision": "5c5c4d39fcb6f4a984ba",
+    "url": "/static/js/main.4514ce13.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
